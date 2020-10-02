d9b60cc99e47ad01f7a0ebc5a56df22182d24cb6
yra3 <64928855+yra3@users.noreply.github.com>
2020-10-03 02:11:18 +0500

    Add first task of 2 work

---
+++ МОАД_Лаб_2_0_СS/МОАД_Лаб_2_0/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace МОАД_Лаб_2_0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(155, 64);
+            this.button1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(155, 172);
+            this.button2.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 60);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(155, 279);
+            this.button3.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(200, 60);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(504, 29);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(975, 774);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(145, 385);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(200, 60);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1517, 861);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "Form1";
+            this.Text = "«Лаб.раб. 2_1. Соловей Юрий";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ МОАД_Лаб_2_0_СS/МОАД_Лаб_2_0/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace МОАД_Лаб_2_0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const int arraySize = 100;
+
+        const string nameBinaryFile = "data.dat";
+
+        const string nameTextFile = "data.txt";
+
+        string NL = Environment.NewLine;
+
+        int[] dateFromBinaryFile = new int[0];
+
+        int[] dateFromTextFile = new int[0];
+
+        int[] createRandomArrayAndOutSumOfElements(out int sum, int length = 100)
+        {
+            int[] a = new int[length];
+            Random random = new Random();
+            sum = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                a[i] = random.Next(100000, 999999);
+                sum += a[i];
+            }
+
+            return a;
+        }
+
+        class SaverArrayToFile
+        {
+            protected int[] date;
+            public SaverArrayToFile(ref int[] array)
+            {
+                date = array;
+            }
+
+            public virtual void saveData(string fileName)
+            {
+            }
+        }
+
+        class SaverArrayToFileBinary : SaverArrayToFile
+        {
+            public SaverArrayToFileBinary(ref int[] array) : base(ref array)
+            {
+            }
+
+            public override void saveData(string fileName)
+            {
+                FileStream fileStream = new FileStream(fileName, FileMode.Create);
+                BinaryWriter binaryWriter = new BinaryWriter(fileStream);
+
+                for (int i = 0; i < date.Length; i++)
+                {
+                    binaryWriter.Write(date[i]);
+                }
+
+                binaryWriter.Close();
+                fileStream.Close();
+            }
+        }
+
+        class SaverArrayToFileText : SaverArrayToFile
+        {
+            public SaverArrayToFileText(ref int[] array) : base(ref array)
+            {
+            }
+
+            public override void saveData(string fileName)
+            {
+                StreamWriter streamWriter = File.CreateText(fileName);
+                for (int i = 99; i >= 0; i--)
+                {
+                    streamWriter.WriteLine(date[i]);
+                }
+                streamWriter.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // create date array
+            int sum;
+            int[] date = createRandomArrayAndOutSumOfElements(out sum);
+
+
+            // print info about creation
+            textBox1.Text = "Данные (100 целых чисел) созданы." + NL
+                + "Общая сумма " + sum.ToString() + "." + NL;
+
+            //save data to binary file
+
+            SaverArrayToFile saverArrayToFile = new SaverArrayToFileBinary(ref date);
+            saverArrayToFile.saveData(nameBinaryFile);
+            
+            textBox1.Text += $"Данные сохранены в \"{nameBinaryFile}\"." + NL;
+
+            // save data to text file
+            saverArrayToFile = new SaverArrayToFileText(ref date);
+            saverArrayToFile.saveData(nameTextFile);
+
+            textBox1.Text += $"Данные сохранены в \"{nameTextFile}\"." + NL;
+            button2.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //read date from binary file
+            FileStream fileStream = new FileStream("data.dat", FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            
+            int count = 0, sum = 0;
+            try
+            {
+                while (true)
+                {
+                    int k = binaryReader.ReadInt32();
+                    count++;
+                    sum += k;
+                    Array.Resize(ref dateFromBinaryFile, count);
+                    dateFromBinaryFile[dateFromBinaryFile.Length - 1] = k;
+                }
+            }
+            catch
+            {
+                textBox1.Text += $"Файл \"{nameBinaryFile}\" считан." + NL;
+            }
+            finally
+            {
+                binaryReader.Close();
+                fileStream.Close();
+            }
+            textBox1.Text += string.Format("Всего {0} чисел, сумма равна {1}.", count, sum) + NL;
+            textBox1.Text += string.Format("a[0]={0} a[{2}]={1}.", dateFromBinaryFile[0], dateFromBinaryFile[count - 1], count - 1) + NL;
+            button3.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = File.OpenText("data.txt");
+            
+            int count = 0, sum = 0;
+            while (true)
+            {
+                string str = sr.ReadLine();
+                if (str == null) break;
+                int k;
+                if (int.TryParse(str, out k))
+                {
+                    count++;
+                    sum += k;
+                    Array.Resize(ref dateFromTextFile, count);
+                    dateFromTextFile[dateFromTextFile.Length - 1] = k;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            sr.Close();
+            textBox1.Text += $"Файл \"{nameTextFile}\" считан." + NL;
+            textBox1.Text += string.Format("Всего {0} чисел, сумма равна {1}.", count, sum) + NL;
+            textBox1.Text += string.Format("a[0]={0} a[2]={1}.", dateFromTextFile[0], dateFromTextFile[count - 1], count - 1) + NL;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int count = 0, sum = 0;
+            int[] c = new int[0];
+
+            for (int i = 0; i< dateFromBinaryFile.Length; i++)
+            {
+                if (dateFromBinaryFile[i]< dateFromTextFile[i])
+                {
+                    count++;
+                    Array.Resize(ref c, count);
+                }
+            }
+
+        }
+    }
+}
